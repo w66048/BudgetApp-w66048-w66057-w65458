39913e1a17ea67c089ab4e458a47e98814083fd8
Kacper <kacper.mazur4@proton.me>
2024-07-02 22:36:30 +0200

    fix

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -22,7 +22,7 @@ export const TransactionList = ({ selectedMonth }) => {
                 filteredTransactions = filteredTransactions.filter(transaction => transaction.type === transactionType);
             }
 
-            setTransactions(filteredTransactions.slice(0, 16).reverse());
+            setTransactions(filteredTransactions.reverse());
         } catch (error) {
             console.error('Error fetching transactions:', error);
         }
